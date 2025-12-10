d8a7718f88f7c01391d3fddb30ece7d4c7dca534
PawelGrzybek <desperatos@gmail.com>
2025-12-10 20:50:46 +0100

    koniec

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 from flask import Flask, render_template, request
 from Manager import Manager
 from dbInit import db
-
+from flask_alembic import Alembic
+# flask --app main2 db revision initial
+# flask --app main2 db upgrade
+# flask --app main2 db revision add_user_type
+# flask --app main2 db upgrade
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 db.init_app(app)
+
+alembic = Alembic()
+alembic.init_app(app)
 counter=0
 
 @app.route('/', methods=['GET'])
